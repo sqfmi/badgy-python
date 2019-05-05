5c5c61f6bdc8d9913f8cd11732b42768479e4817
sqfmi <sqfmi@github.com>
2019-05-05 18:05:17 -0400

    updated image and script

---
+++ main.py
@@ -0,0 +1,22 @@
+import epaper2in9
+from machine import Pin, SPI
+
+# SPI1 on ESP8266
+spi  = SPI(1, baudrate=80000000, polarity=0, phase=0)
+cs   = Pin(15, Pin.OUT)
+dc   = Pin(0, Pin.OUT)
+rst  = Pin(2, Pin.OUT)
+busy = Pin(4, Pin.OUT)
+
+e = epaper2in9.EPD(spi, cs, dc, rst, busy)
+e.init()
+
+w = 128
+h = 296
+x = 0
+y = 0
+
+from image import micropython_logo
+e.clear_frame_memory(0xFF)
+e.set_frame_memory(hello, x, y, w, h)
+e.display_frame()
